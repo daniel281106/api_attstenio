49a413e1cd86f9093bcfcd104229fe7de523348e
daniel281106 <danielgti28@gmail.com>
2023-11-07 22:32:35 -0300

    second commit

--- src/controllers/usersloginController.js
+++ src/controllers/usersloginController.js
@@ -4,36 +4,41 @@ const { hash, compare } = require("bcryptjs");
 const knex = require("../database/knex");
 const bcrypt = require("bcryptjs");
 
-class UsersLoginController{
-    async login(req,res){
-        const { email, password } = req.body;
+class UsersLoginController {
+  async login(req, res) {
+    const { email, password } = req.body;
 
-  knex.select('email', 'password')
-    .from('users')
-    .where('email', '=', email)
-    .then(data => {
-      if (data.length === 1) {
-        const isValid = bcrypt.compareSync(password, data[0].password);
-        if (isValid) {
-          const token = jwt.sign({ email }, 'seuSegredo');
-          console.log(res.json(token));
+    knex.select('email', 'password')
+      .from('users')
+      .where('email', '=', email)
+      .then(data => {
+        if (data.length === 1) {
+          const isValid = bcrypt.compareSync(password, data[0].password);
+          if (isValid) {
+            const token = jwt.sign({ email }, 'seuSegredo');
+            res.json(token);
+          } else {
+            res.status(400).json('Credenciais incorretas');
+          }
         } else {
           res.status(400).json('Credenciais incorretas');
         }
-      } else {
-        res.status(400).json('Credenciais incorretas');
-      }
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(400).json('Erro no login');
-    });
-    }
-    async show(req, res){
-      const { token } = req.body;
-        res.json(token)
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(400).json('Erro no login');
+      });
+  }
+  async show(req, res) {
+    const { email, password } = req.body;
+    knex.select('email', 'password')
+      .from('users')
+      .where('email', '=', email)
+    const token = jwt.sign({ email }, 'seuSegredo');
+    res.json(token);
 
-}
+
+  }
 };
 
 module.exports = UsersLoginController;
